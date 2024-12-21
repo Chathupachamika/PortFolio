4f3a95e69ab55363493dc9e20c361be621e42e2d
Chathupachamika <chathupachamika765@gmail.com>
2024-12-21 13:00:11 +0530

    fifth commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,21 +8,19 @@ import AOS from 'aos';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  // AOS initialization in ngOnInit lifecycle hook
   ngOnInit() {
     AOS.init({
-      duration: 1200, // Adjust duration as needed
-      once: true      // Ensures animations happen only once
+      duration: 1200, 
+      once: true      
     });
   }
   downloadCV(): void {
-    const cvPath = '/assets/cv/ATS CV Resume.pdf'; // Ensure this path is correct for your assets
+    const cvPath = 'C:/ICD110/web%20developing/PortFolio-main/PortFolio-main/src/assets/cv/ATS%20CV%20Resume.pdf'; 
     const link = document.createElement('a');
     link.href = cvPath;
     link.download = 'ATS CV Resume.pdf';
-    document.body.appendChild(link); // Temporarily add the link to the DOM
-    link.click(); // Trigger download
-    document.body.removeChild(link); // Remove the link to clean up
+    document.body.appendChild(link); 
+    link.click(); 
+    document.body.removeChild(link); 
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/about', pathMatch: 'full' }, // Default route, redirects to About
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'education', component: EducationComponent },
 ];
