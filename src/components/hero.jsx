8dafe0649b6a7d4460b802f8dd0b4dc3f9a96bf6
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-04-26 00:55:55 +0530

    Update hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -13,7 +13,6 @@ export default function Hero() {
     animate: { opacity: 1, y: 0 },
     transition: { duration: 0.8 }
   };
-
   const slideIn = {
     initial: { x: 100, opacity: 0 },
     animate: { x: 0, opacity: 1 },
